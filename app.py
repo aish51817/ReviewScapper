56fba9e762eb293e18d064357c045659bcdbc5ad
aish51817 <aishwarya.srivastava1177.com>
2022-04-10 12:22:57 +0530

    this is my first proj

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS,cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
+from selenium import webdriver
 
 app = Flask(__name__)
 
